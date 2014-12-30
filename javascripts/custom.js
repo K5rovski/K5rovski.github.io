289869b97e5072a60295fcdc174ef0a88bfc389d
K5rovski <glupoime101@gmail.com>
2014-12-30 12:25:21 +0100

    test json

---
+++ javascripts/custom.js
@@ -0,0 +1,26 @@
+ window.onload = function () {
+ 
+		var data=$.getJSON('params.json');
+          var chart = new CanvasJS.Chart("chartContainer", {
+              theme: "theme2",//theme1
+              title:{
+                  text: "Basic Column Chart - CanvasJS"              
+             },
+              data: [              
+              {
+// Change type to "bar", "splineArea", "area", "spline", "pie",etc.
+                  type: "line",
+				  markerType:"none",
+                  dataPoints: [
+                  {  y: 10 },
+                  {  y: 15 },
+                  { label: "banana", y: 25 },
+                  { label: "mango", y: 30 },
+                  { label: "grape", y: 28 }
+                  ]
+              }
+              ]
+          });
+
+          chart.render();
+      }
